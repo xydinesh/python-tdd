ecf5ade4f2dd63e936ef6cbd636d1ec6d482981a
Dinesh Weerapurage <xydinesh@gmail.com>
2014-11-29 03:07:30 -0500

    git commit -am"getting ready to refactor FT"

--- DjangoTDD/functional_tests.py
+++ DjangoTDD/functional_tests.py
@@ -34,10 +34,14 @@ class NewVisitorTest(unittest.TestCase):
     inputbox.send_keys(Keys.ENTER)
     # send_keys is selenium way of input text
     
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use peacock feathers to make a fly')
+    inputbox.send_keys(Keys.ENTER)
 
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
-    self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New To-Do item not appear in the table")
+    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+    self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly"
