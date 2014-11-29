627670fd061570eb07d35c22715cff6269d98b9a
Dinesh Weerapurage <xydinesh@gmail.com>
2014-11-29 01:57:00 -0500

    Refactor home page view to user a template

--- DjangoTDD/lists/tests.py
+++ DjangoTDD/lists/tests.py
@@ -10,6 +10,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
@@ -29,7 +30,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_string = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_string)
 
--- DjangoTDD/lists/views.py
+++ DjangoTDD/lists/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
-    return HttpResponse(b'<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
     
