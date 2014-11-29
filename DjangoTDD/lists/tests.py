bf460eda8ba0fe227c6675fc838a21a70ca2b37a
Dinesh Weerapurage <xydinesh@gmail.com>
2014-11-29 01:48:56 -0500

    Basic view now returns minimal HTML

--- DjangoTDD/lists/tests.py
+++ DjangoTDD/lists/tests.py
@@ -8,6 +8,7 @@ Replace this with more appropriate tests for your application.
 import django
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from lists.views import home_page
 
 # TODO: Configure your database in settings.py and sync before running tests.
@@ -24,4 +25,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
        found = resolve('/')
        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
 
--- DjangoTDD/lists/views.py
+++ DjangoTDD/lists/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse(b'<html><title>To-Do lists</title></html>')
+    
