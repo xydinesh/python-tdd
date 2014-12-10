33edb521a6ccf381cb7beb8b0884fe5d4d14a99f
Dinesh Weerapurage <xydinesh@gmail.com>
2014-12-10 07:42:58 -0500

    moved functional tests into a module and using Django live server tests

--- DjangoTDD/functional_tests/tests.py
+++ DjangoTDD/functional_tests/tests.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
   def setUp(self):
     self.browser = webdriver.Firefox()
@@ -13,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
 
   def test_can_start_a_list_and_retrieve_it_later(self):
     
-    self.browser.get('http://localhost:8000')
+    self.browser.get(self.live_server_url)
 
     # She notices the page title and head mention to-do lists
     self.assertIn('To-Do', self.browser.title)
@@ -52,6 +53,3 @@ class NewVisitorTest(unittest.TestCase):
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
     return self.assertIn(row_text, [row.text for row in rows])
-
-if __name__ == '__main__':
-  unittest.main(warnings='ignore')
