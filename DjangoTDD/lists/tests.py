b3723d4dbf1e745f2b301179e2c63e80aaa79079
Dinesh Weerapurage <xydinesh@gmail.com>
2014-12-30 07:25:55 -0500

    adding new url for saving list items

--- DjangoTDD/lists/tests.py
+++ DjangoTDD/lists/tests.py
@@ -33,33 +33,24 @@ class HomePageTest(TestCase):
         expected_string = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_string)
 
-    def test_home_page_can_save_a_post_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-        response = home_page(request)
-
+class NewListTest(TestCase):
+    def test_saving_a_post_request(self):
+        self.client.post(
+        '/lists/new',
+        data={'item_text': 'A new list item'}
+        )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertIn('A new list item', new_item.text)
 
-
     def test_redirects_after_post(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
+        response = self.client.post(
+        '/lists/new',
+        data={'item_text': 'A new list item'}
+        )
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-    def test_home_page_only_when_necessary(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-        response = home_page(request)
-        self.assertEqual(Item.objects.count(), 1)
 
 class ItemModelTest(TestCase):
 
@@ -79,6 +70,7 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
 
 class ListViewTest(TestCase):
 
