1bcdfb92d6f82c19380596f35422b4c4d100c282
Dinesh Weerapurage <xydinesh@gmail.com>
2015-01-04 18:04:16 -0500

    moving url into lists and global url for matching lists

--- DjangoTDD/DjangoTDD/urls.py
+++ DjangoTDD/DjangoTDD/urls.py
@@ -6,16 +6,14 @@ from datetime import datetime
 from django.conf.urls import patterns, url
 
 # Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
+from django.conf.urls import include
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list')
+    url(r'^lists/', include('lists.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
